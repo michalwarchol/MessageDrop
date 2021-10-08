94e0dc8659d732cd58cb90ea0c032efb4d26b6da
Michał Warchoł <imsimilar@gmail.com>
2021-10-08 22:47:34 +0200

    :bearded_person: Added basic fields

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,17 +1,47 @@
-import {getModelForClass, mongoose, prop as Property, } from "@typegoose/typegoose";
+import {
+  getModelForClass,
+  mongoose,
+  prop as Property,
+} from "@typegoose/typegoose";
 import { GraphQLID } from "graphql";
 import { Field, ObjectType } from "type-graphql";
 
 @ObjectType()
 export class User {
+  @Field(() => GraphQLID)
+  readonly _id: mongoose.Types.ObjectId;
 
-    @Field(()=>GraphQLID)
-    readonly _id: mongoose.Types.ObjectId
+  @Field()
+  @Property({ type: () => String, unique: true, minlength: 6 })
+  name: string;
 
-    @Field()
-    @Property({type:() => String, unique: true})
-    name: string;
+  @Property({ type: () => String })
+  password: string;
+
+  @Field()
+  @Property({ type: () => String, unique: true })
+  email: string;
+
+  @Field({ nullable: true })
+  @Property({ type: () => String, required: false, default: null })
+  avatarId: string;
+
+  @Field(() => Boolean)
+  @Property({ type: () => Boolean, default: false })
+  verified: boolean;
+
+  @Field()
+  createdAt: Date;
+
+  @Field()
+  updatedAt: Date;
 }
 
-export const UserModel = getModelForClass(User);
-
+export const UserModel = getModelForClass(User, {
+  schemaOptions: {
+    timestamps: {
+      createdAt: true,
+      updatedAt: true,
+    },
+  },
+});
