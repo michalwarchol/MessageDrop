b237aa626b114901a52d722e0697c12943727118
Michał Warchoł <imsimilar@gmail.com>
2021-10-08 22:48:23 +0200

    :bicyclist: Added session

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,17 +2,47 @@ import "dotenv-safe/config";
 import "reflect-metadata";
 import mongoose from "mongoose";
 import express from "express";
+import Redis from "ioredis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { UserResolver } from "./resolvers/UserResolver";
+import { COOKIE_NAME, __prod__ } from "./constants";
 
 const main = async () => {
-  await mongoose.connect("mongodb://localhost:27017", {dbName: process.env.DB_NAME});
+  await mongoose.connect(process.env.DB_URL, {
+    dbName: process.env.DB_NAME,
+  });
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redis = new Redis();
+
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({ client: redis, disableTouch: true }),
+      saveUninitialized: false,
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 30, //1 month
+        httpOnly: true,
+        sameSite: "lax",
+        secure: __prod__,
+      },
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({ resolvers: [UserResolver], validate: false }),
+    context: ({req, res}) => ({
+      req,
+      res,
+      redis
+    })
   });
 
   await apolloServer.start();
