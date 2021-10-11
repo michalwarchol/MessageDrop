be177b18647065040c6a80ae8481b5382d4a88f5
Michał Warchoł <imsimilar@gmail.com>
2021-10-11 22:50:35 +0200

    :boom: Created web ui

---
+++ web/src/components/Wrapper/Wrapper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "./Wrapper.module.scss";
+
+const Wrapper:React.FC = () => {
+    return(
+        <div className={styles.wrapper}>wrapper</div>
+    )
+}
+export default Wrapper;
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Wrapper from "../components/Wrapper/Wrapper";
+
+export default function Home() {
+  return (
+    <div>
+      hello world123
+      <Wrapper />
+    </div>
+  )
+}
