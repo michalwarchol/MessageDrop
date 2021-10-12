8ba156427eaaa6276137a6ab4204e49b19b1fa79
Michał Warchoł <imsimilar@gmail.com>
2021-10-12 23:19:15 +0200

    :a: Added apollo provider and Poppins font

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,8 +1,29 @@
-import './styles.scss'
-import {AppProps} from 'next/app'
+import "../styles/styles.scss";
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+  credentials: "include"
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
+      </Head>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
