5d9275a77926623c16080051150133f4ac87d1b4
Michał Warchoł <imsimilar@gmail.com>
2021-10-12 23:14:46 +0200

    :keyboard: Created custom input field

---
+++ web/src/components/InputField/InputField.tsx
@@ -0,0 +1,19 @@
+import { useField } from 'formik';
+import React, { InputHTMLAttributes } from 'react'
+import styles from "./InputField.module.scss";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+    name: string;
+    label: string;
+}
+
+const InputField:React.FC<InputFieldProps> = ({label, ...props}) => {
+    const [field, {error}] = useField(props);
+    return(
+        <div className={styles.inputContainer}>
+            <input className={styles.inputField} {...field} {...props} />
+            {error && <p>{error}</p>}
+        </div>
+    )
+}
+export default InputField;
