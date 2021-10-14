f90827912027763872f74bcd8ba7bee0e38281d8
Michał Warchoł <imsimilar@gmail.com>
2021-10-14 22:48:13 +0200

    :green_circle: Created custom radio button

---
+++ web/src/components/InlineRadio/InlineRadio.tsx
@@ -0,0 +1,24 @@
+import { useField } from "formik";
+import React, { InputHTMLAttributes } from "react";
+import styles from "./InlineRadio.module.scss";
+
+type RadioProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+};
+
+const InlineRadio: React.FC<RadioProps> = (props) => {
+  const [field] = useField(props);
+  console.log(props.value, props.checked)
+  
+  return (
+    <label htmlFor={props.id} className={props.checked ? styles.inlineRadioFill : styles.inlineRadioOutline}>
+        {props.value}
+      <input
+        type="radio"
+        {...field}
+        {...props}
+      />
+    </label>
+  );
+};
+export default InlineRadio;
