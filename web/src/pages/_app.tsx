055468ef42fb48c14847a7cc91ae03477944b33b
Michał Warchoł <imsimilar@gmail.com>
2021-10-15 21:16:53 +0200

    :package: Added apollo-upload-client to enable file uploads

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,10 +2,15 @@ import "../styles/styles.scss";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
+  link: createUploadLink({
+    uri: 'http://localhost:4000/graphql',
+    credentials: "include"
+  }),
   credentials: "include"
 });
 
