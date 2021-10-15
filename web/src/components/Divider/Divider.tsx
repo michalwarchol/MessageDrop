072522705ec60c0bce4a4e5d8a44dc4f535d28cd
Michał Warchoł <imsimilar@gmail.com>
2021-10-15 21:25:08 +0200

    :card_index_dividers: Created simple divider

---
+++ web/src/components/Divider/Divider.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "./Divider.module.scss";
+
+const Divider:React.FC = () => {
+    return(
+        <div className={styles.divider}></div>
+    )
+}
+export default Divider;
