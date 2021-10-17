d0c39486eeca92d2bb9a597775b00183264d27f6
Michał Warchoł <imsimilar@gmail.com>
2021-10-17 23:19:00 +0200

    :link: Created chat room shortcuts in the sidebar

---
+++ web/src/components/ChatRoomShortcut/ChatRoomShortcut.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./ChatRoomShortcut.module.scss";
+import { ChatRoomWithImage } from "../../generated/graphql";
+import Image from "next/image";
+import { base64ToObjectURL } from "../../utils/base64ToObjectURL";
+import { BsFillChatDotsFill } from "react-icons/bs";
+
+interface Props {
+  chatRoomWithImage: ChatRoomWithImage;
+}
+
+const ChatRoomShortCut: React.FC<Props> = ({ chatRoomWithImage }) => {
+  return (
+    <div className={styles.chatRoomShortcut}>
+      {chatRoomWithImage.image ? (
+        <div className={styles.imageOrIcon}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={base64ToObjectURL(chatRoomWithImage.image)}
+              width={50}
+              height={50}
+            />
+          </div>
+        </div>
+      ) : (
+        <div className={styles.imageOrIcon}>
+          <div className={styles.imageContainer}>
+            <BsFillChatDotsFill />
+          </div>
+        </div>
+      )}
+      <div className={styles.info}>
+        <p className={styles.chatName}>{chatRoomWithImage.chatRoom.name}</p>
+      </div>
+    </div>
+  );
+};
+export default ChatRoomShortCut;
--- web/src/components/ChatSidebar/ChatSidebar.tsx
+++ web/src/components/ChatSidebar/ChatSidebar.tsx
@@ -2,14 +2,23 @@ import React from "react";
 import styles from "./ChatSidebar.module.scss";
 import Divider from "../Divider/Divider";
 import CreateChatButton from "../CreateChatButton/CreateChatButton";
+import { useGetCreatorChatRoomsQuery } from "../../generated/graphql";
+import ChatRoomShortCut from "../ChatRoomShortcut/ChatRoomShortcut";
 
 const ChatSidebar: React.FC = () => {
+
+  const {data} = useGetCreatorChatRoomsQuery();
+
   return (
     <div className={styles.chatSidebar}>
       <div className={styles.create}>
         <CreateChatButton />
       </div>
       <Divider />
+      <div><p>Your chats</p></div>
+      <div className={styles.userRooms}>
+        {data?.getCreatorChatRooms.map((elem, index) => (<ChatRoomShortCut chatRoomWithImage={elem} key={index} />))}
+      </div>
     </div>
   );
 };
