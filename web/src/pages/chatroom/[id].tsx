a8107a842b51f7362ce1794193137ed7917e01dc
Michał Warchoł <imsimilar@gmail.com>
2021-10-25 00:00:41 +0200

    :door: Created chatroom page

---
+++ web/src/pages/chatroom/[id].tsx
@@ -0,0 +1,42 @@
+import { NextPage } from "next";
+import React from "react";
+import styles from "./ChatRoom.module.scss";
+import Wrapper from "../../components/Wrapper/Wrapper";
+import { useIsChatMember } from "../../utils/useIsChatMember";
+import Navbar from "../../components/Navbar/Navbar";
+import { withApollo } from "../../utils/withApollo";
+import { useRouter } from "next/router";
+import ChatRoomContent from "../../components/ChatRoomContent/ChatRoomContent";
+import { RoomContext } from "../../utils/RoomContext";
+
+interface Props {
+  id?: string;
+}
+
+const ChatRoom: NextPage<Props> = ({ id }) => {
+  const router = useRouter();
+  if (id) {
+    useIsChatMember(id);
+  } else {
+    router.back();
+  }
+
+  return (
+    <RoomContext.Provider value={id || ""}>
+      <Wrapper size="lg">
+        <div className={styles.chatRoom}>
+          <Navbar />
+          <ChatRoomContent />
+        </div>
+      </Wrapper>
+    </RoomContext.Provider>
+  );
+};
+
+ChatRoom.getInitialProps = async ({ query }) => {
+  return {
+    id: query.id as string,
+  };
+};
+
+export default withApollo()(ChatRoom);
