06c29377aae370bbb84a71194af0cfba685cdcbd
Michał Warchoł <imsimilar@gmail.com>
2021-10-24 23:58:26 +0200

    :bellhop_bell: Added merge function on getRoomMessages query to join messages arrays

--- web/src/utils/withApollo.ts
+++ web/src/utils/withApollo.ts
@@ -1,10 +1,29 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
 import { withApollo as createWithApollo } from "next-apollo";
+import { PaginatedMessages } from "../generated/graphql";
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000/graphql',
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            getRoomMessages: {
+              keyArgs: [],
+              merge(
+                existing: PaginatedMessages | undefined,
+                incoming: PaginatedMessages
+              ): PaginatedMessages {
+                return {...incoming,
+                  messages: [...incoming.messages, ...(existing?.messages || [])]
+                }
+              }
+            }
+          }
+        }
+      }
+    }),
     link: createUploadLink({
       uri: 'http://localhost:4000/graphql',
       credentials: "include"
