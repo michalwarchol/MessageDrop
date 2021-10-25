2c8e7a53d12672e10f8113044adb961a7496e440
Michał Warchoł <imsimilar@gmail.com>
2021-10-25 19:37:52 +0200

    :hammer: Added media and files display; Added division on users messages and other's messages

--- web/src/components/Chat/Chat.tsx
+++ web/src/components/Chat/Chat.tsx
@@ -5,7 +5,6 @@ import {
   useGetRoomMessagesQuery,
 } from "../../generated/graphql";
 import { RoomContext } from "../../utils/RoomContext";
-import Wrapper from "../Wrapper/Wrapper";
 import styles from "./Chat.module.scss";
 import { Formik, Form } from "formik";
 import InputField from "../InputField/InputField";
@@ -21,139 +20,154 @@ import { base64ToObjectURL } from "../../utils/base64ToObjectURL";
 import MessageNode from "../MessageNode/MessageNode";
 
 const Chat: React.FC = () => {
-
   const roomId = useContext(RoomContext);
 
   const [text, setText] = useState<string>("");
   const [media, setMedia] = useState<File | null>(null);
   const [file, setFile] = useState<File | null>(null);
-  const [variables, setVariables] = useState({limit: 2, roomId, skip: null});
 
   const { data: room } = useGetChatRoomByIdQuery({ variables: { roomId } });
-  const { data, fetchMore } = useGetRoomMessagesQuery({variables});
+  const { data, fetchMore } = useGetRoomMessagesQuery({
+    variables: { limit: 2, roomId, skip: null },
+  });
   const [createMessage] = useCreateMessageMutation();
 
   const mediaInputRef = useRef<HTMLInputElement>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <Wrapper size="md">
-      <div className={styles.chat}>
-        <div className={styles.chatInfo}>
-          {room?.getChatRoomById.image ? (
-            <div className={styles.imageOrIcon}>
-              <div className={styles.imageContainer}>
-                <Image
-                  src={base64ToObjectURL(room.getChatRoomById.image)}
-                  width={100}
-                  height={100}
-                />
-              </div>
+    <div className={styles.chat}>
+      <div className={styles.chatInfo}>
+        {room?.getChatRoomById.image ? (
+          <div className={styles.imageOrIcon}>
+            <div className={styles.imageContainer}>
+              <Image
+                src={base64ToObjectURL(room.getChatRoomById.image)}
+                width={100}
+                height={100}
+              />
             </div>
-          ) : (
-            <div className={styles.imageOrIcon}>
-              <div className={styles.imageContainer}>
-                <BsFillChatDotsFill />
-              </div>
-            </div>
-          )}
-          <div className={styles.chatHeaders}>
-            <p className={styles.chatName}>
-              {room?.getChatRoomById.chatRoom.name}
-            </p>
-            <p className={styles.chatDescription}>
-              {room?.getChatRoomById.chatRoom.description}
-            </p>
           </div>
-        </div>
-        <div className={styles.chatMessages}>
-          {data && data.getRoomMessages.messages.map((elem, index)=>(
-            <MessageNode message={elem} key={index} />
-          ))}
-          <button onClick={()=>{
-            fetchMore({variables: {
-              roomId,
-              limit: 2,
-              skip: data?.getRoomMessages.messages.length
-            }})
-          }}>load more</button>
-        </div>
-        <div className={styles.chatForm}>
-          <Formik
-            initialValues={{}}
-            onSubmit={async () => {
-              if (!text && !file && !media) {
-                return;
-              }
-
-              await createMessage({
-                variables: {
-                  roomId,
-                  text,
-                },
-              });
-            }}
-          >
-            {() => (
-              <Form className={styles.chatFormFields}>
-                <InputField
-                  name="text"
-                  value={text}
-                  onChange={(e) => setText(e.currentTarget.value)}
-                  placeholder="Write something..."
-                />
-                <label htmlFor="media">
-                  <IconButton
-                    Icon={BsFileImageFill}
-                    variant="outline"
-                    className={styles.actionButton}
-                    onClick={() => {
-                      mediaInputRef.current?.click();
-                    }}
-                  />
-                  <input
-                    type="file"
-                    id="media"
-                    accept="image/png, image/jpeg"
-                    ref={mediaInputRef}
-                    className={styles.fileInput}
-                    onChange={(e) => {
-                      if(e.target.files!=null)
-                        setMedia(e.target.files[0])
-                    }}
-                  />
-                </label>
-                <label htmlFor="file">
-                  <IconButton
-                    Icon={BsFillFileEarmarkPlusFill}
-                    variant="outline"
-                    className={styles.actionButton}
-                    onClick={() => {
-                      fileInputRef.current?.click();
-                    }}
-                  />
-                  <input
-                    type="file"
-                    id="file"
-                    ref={fileInputRef}
-                    className={styles.fileInput}
-                    onChange={(e) => {
-                      if(e.target.files!=null)
-                        setFile(e.target.files[0])
-                    }}
-                  />
-                </label>
-                <IconButton
-                  Icon={BsEmojiLaughingFill}
-                  variant="outline"
-                  className={styles.actionButton}
-                />
-              </Form>
-            )}
-          </Formik>
+        ) : (
+          <div className={styles.imageOrIcon}>
+            <div className={styles.imageContainer}>
+              <BsFillChatDotsFill />
+            </div>
+          </div>
+        )}
+        <div className={styles.chatHeaders}>
+          <p className={styles.chatName}>
+            {room?.getChatRoomById.chatRoom.name}
+          </p>
+          <p className={styles.chatDescription}>
+            {room?.getChatRoomById.chatRoom.description}
+          </p>
         </div>
       </div>
-    </Wrapper>
+      <div className={styles.chatMessages}>
+        {data &&
+          data.getRoomMessages.messages.map((elem, index, arr) => {
+            let newUser;
+            if (index == 0) {
+              newUser = true;
+            } else {
+              newUser =
+                arr[index - 1].message.creatorId != elem.message.creatorId;
+            }
+            return <MessageNode message={elem} key={index} newUser={newUser} />;
+          })}
+        <button
+          onClick={() => {
+            fetchMore({
+              variables: {
+                roomId,
+                limit: 2,
+                skip: data?.getRoomMessages.messages.length,
+              },
+            });
+          }}
+        >
+          load more
+        </button>
+      </div>
+      <div className={styles.chatForm}>
+        <Formik
+          initialValues={{}}
+          onSubmit={async (_, { resetForm }) => {
+            if (!text && !file && !media) {
+              return;
+            }
+
+            await createMessage({
+              variables: {
+                roomId,
+                text,
+                media,
+                file,
+              },
+            });
+            resetForm();
+          }}
+        >
+          {() => (
+            <Form className={styles.chatFormFields}>
+              <InputField
+                name="text"
+                value={text}
+                onChange={(e) => setText(e.currentTarget.value)}
+                placeholder="Write something..."
+              />
+              <label htmlFor="media">
+                <IconButton
+                  Icon={BsFileImageFill}
+                  variant="outline"
+                  className={styles.actionButton}
+                  type="button"
+                  onClick={() => {
+                    mediaInputRef.current?.click();
+                  }}
+                />
+                <input
+                  type="file"
+                  id="media"
+                  accept="image/png, image/jpeg"
+                  ref={mediaInputRef}
+                  className={styles.fileInput}
+                  onChange={(e) => {
+                    if (e.target.files != null) setMedia(e.target.files[0]);
+                  }}
+                />
+              </label>
+              <label htmlFor="file">
+                <IconButton
+                  Icon={BsFillFileEarmarkPlusFill}
+                  variant="outline"
+                  className={styles.actionButton}
+                  type="button"
+                  onClick={() => {
+                    fileInputRef.current?.click();
+                  }}
+                />
+                <input
+                  type="file"
+                  id="file"
+                  ref={fileInputRef}
+                  className={styles.fileInput}
+                  onChange={(e) => {
+                    if (e.target.files != null) setFile(e.target.files[0]);
+                  }}
+                />
+              </label>
+              <IconButton
+                Icon={BsEmojiLaughingFill}
+                variant="outline"
+                className={styles.actionButton}
+              />
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
   );
 };
 export default Chat;
--- web/src/components/MessageNode/MessageNode.tsx
+++ web/src/components/MessageNode/MessageNode.tsx
@@ -1,16 +1,66 @@
-import React from 'react'
-import { Message } from '../../generated/graphql';
+import React, { useState } from "react";
+import {
+  MessageWithMedia,
+  useGetUserByIdQuery,
+  useMeQuery,
+} from "../../generated/graphql";
 import styles from "./MessageNode.module.scss";
+import NextImage from "next/image";
+import { base64ToObjectURL } from "../../utils/base64ToObjectURL";
+import FileDownloader from "../FileDownloader/FileDownloader";
 
 interface Props {
-    message: Message;
+  message: MessageWithMedia;
+  newUser: boolean;
 }
 
-const MessageNode:React.FC<Props> = ({message}) => {
-    return(
-        <div className={styles.messageNode}>
-            {message.text}
+const MessageNode: React.FC<Props> = ({ message, newUser }) => {
+  const [proportion, setProportion] = useState<number>(1);
+
+  const { data: me } = useMeQuery();
+  const { data: creator } = useGetUserByIdQuery({
+    variables: { userId: message.message.creatorId },
+  });
+
+  let position = "start";
+  let backgroundColor = "#4C5057";
+  if (me?.me?._id == message.message.creatorId) {
+    position = "end";
+    backgroundColor = "#0e5bcf";
+  }
+
+  if(message.media){
+    let img = new Image();
+    img.src = base64ToObjectURL(message.media);
+    img.onload = () => {
+      setProportion(img.height / img.width);
+    };
+  }
+  return (
+    <div
+      className={styles.messageNode}
+      style={{
+        alignSelf: position,
+        justifyContent: position,
+        marginTop: newUser ? "10px" : "4px",
+      }}
+    >
+      {newUser && me?.me?._id != message.message.creatorId && (
+        <div className={styles.creator}>{creator?.getUserById.user.name}</div>
+      )}
+      <div className={styles.actualMessage} style={{ backgroundColor }}>
+        {message.message.text}
+      </div>
+      {message.media && (
+        <div className={styles.media}>
+          <NextImage width={180} height={180*proportion} src={base64ToObjectURL(message.media)} />
         </div>
-    )
-}
+      )}
+
+      {message.file && message.message.fileData && (
+          <FileDownloader href={message.file} fileData={message.message.fileData} />
+      )}
+    </div>
+  );
+};
 export default MessageNode;
