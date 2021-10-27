aa3e28aba1ab7d3e39141d00727ffdf7b006fb7a
Michał Warchoł <imsimilar@gmail.com>
2021-10-27 23:30:36 +0200

    :link: Created WebSocketLink to handle subscriptions and added subscription response to cache

--- web/src/utils/withApollo.ts
+++ web/src/utils/withApollo.ts
@@ -1,7 +1,38 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, split } from "@apollo/client";
+import {WebSocketLink} from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 import { createUploadLink } from "apollo-upload-client";
 import { withApollo as createWithApollo } from "next-apollo";
 import { PaginatedMessages } from "../generated/graphql";
+import { isServer } from "./isServer";
+
+const wsLink = isServer ? new WebSocketLink({
+  uri: 'ws://localhost:4000/graphql',
+  options: {
+    reconnect: true,
+    connectionParams: {
+      credentials: "include"
+    }
+  }
+}) : null;
+
+const uploadLink = createUploadLink({
+  uri: 'http://localhost:4000/graphql',
+  credentials: "include"
+});
+
+const splitLink = isServer ? split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink!,
+  uploadLink
+) : uploadLink;
+
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000/graphql',
@@ -10,11 +41,16 @@ const client = new ApolloClient({
         Query: {
           fields: {
             getRoomMessages: {
-              keyArgs: [],
+              keyArgs: ["roomId"],
               merge(
                 existing: PaginatedMessages | undefined,
                 incoming: PaginatedMessages
               ): PaginatedMessages {
+                if(incoming.isSubFeed){
+                  return {...incoming,
+                    messages: [...(existing?.messages || []), ...incoming.messages]
+                  }
+                }
                 return {...incoming,
                   messages: [...incoming.messages, ...(existing?.messages || [])]
                 }
@@ -24,10 +60,7 @@ const client = new ApolloClient({
         }
       }
     }),
-    link: createUploadLink({
-      uri: 'http://localhost:4000/graphql',
-      credentials: "include"
-    }),
+    link: splitLink,
     credentials: "include"
   });
 
