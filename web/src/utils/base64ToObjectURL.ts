da237aae361b6352de7f5c804f5def3d03cfba72
Michał Warchoł <imsimilar@gmail.com>
2021-10-29 23:50:30 +0200

    :adhesive_bandage: Used Buffer.from instead of deprecated atob()

--- web/src/utils/base64ToObjectURL.ts
+++ web/src/utils/base64ToObjectURL.ts
@@ -1,6 +1,6 @@
 export const base64ToObjectURL = (base64: string): string => {
     if(base64){
-        let arrayBufferView = Uint8Array.from(window.atob(base64), c => c.charCodeAt(0));
+        let arrayBufferView = Uint8Array.from(Buffer.from(base64, "base64"), c => c);
         let blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
         let objectURL = URL.createObjectURL(blob);
         return objectURL;
