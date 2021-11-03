7e3a902fdfdeba51c4512070f7fec9385d38a800
Michał Warchoł <imsimilar@gmail.com>
2021-11-03 16:22:06 +0100

    :link: Added routes to /home and /logout

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -4,19 +4,25 @@ import IconButton from '../IconButton/IconButton';
 import styles from "./Navbar.module.scss";
 import {AiFillHome, AiOutlineSearch} from "react-icons/ai";
 import {IoMdPower, IoMdSettings} from "react-icons/io";
+import { useRouter } from 'next/router';
 
 const Navbar:React.FC = () => {
 
     const { data } = useMeQuery();
+    const router = useRouter();
 
     return(
         <div className={styles.navbar}>
             <div className={styles.name}>{data?.me?.name}</div>
             <div className={styles.shortcuts}>
-                <IconButton Icon={AiFillHome} className={styles.iconButtonStyle} />
+                <IconButton Icon={AiFillHome} className={styles.iconButtonStyle} onClick={()=>{
+                    router.push("/home");
+                }} />
                 <IconButton Icon={AiOutlineSearch} className={styles.iconButtonStyle} />
                 <IconButton Icon={IoMdSettings} className={styles.iconButtonStyle} />
-                <IconButton Icon={IoMdPower} className={styles.iconButtonStyle} />
+                <IconButton Icon={IoMdPower} className={styles.iconButtonStyle} onClick={async ()=>{
+                    router.push("/logout");
+                }} />
             </div>
         </div>
     )
