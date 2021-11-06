6d30a2887a174d9321156cbc17e3a97c6cc21b41
Michał Warchoł <imsimilar@gmail.com>
2021-11-06 16:06:30 +0100

    :repeat: Created loading indicator

---
+++ web/src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "./LoadingIndicator.module.scss";
+
+const LoadingIndicator:React.FC = () => {
+    return(
+        <div className={styles.loadingIndicator}></div>
+    )
+}
+export default LoadingIndicator;
