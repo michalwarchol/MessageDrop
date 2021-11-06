dbc1f7f824155315713222f26e7181e5968b462a
Michał Warchoł <imsimilar@gmail.com>
2021-11-06 15:56:07 +0100

    :card_file_box: Created cache update after adding a user to a chat room

---
+++ web/src/cacheModifications/addUserToChatRoomUpdate.ts
@@ -0,0 +1,61 @@
+import { ApolloCache } from "@apollo/client";
+import { gql } from "graphql-tag";
+import {
+  GetChatRoomUsersDocument,
+  GetChatRoomUsersQuery,
+  UserWithAvatar,
+} from "../generated/graphql";
+
+export const addUserToChatRoomUpdate = (
+  userWithAvatar: UserWithAvatar,
+  roomId: string
+) => {
+  return (cache: ApolloCache<any>, { data }: any) => {
+    if (!data) {
+      return;
+    }
+    //reading updated chat room
+    const chatRoom: any = cache.readFragment({
+      id: "ChatRoom:" + roomId,
+      fragment: gql`
+        fragment _ on ChatRoom {
+          _id
+          userIds
+        }
+      `,
+    });
+
+    let newUserIds = [...chatRoom.userIds, userWithAvatar.user._id];
+
+    //overwriting updated chatRoom
+    cache.writeFragment({
+      id: "ChatRoom:" + roomId,
+      fragment: gql`
+        fragment ChangeUserPermissions on ChatRoom {
+          userIds
+        }
+      `,
+      data: { userIds: newUserIds },
+    });
+
+    //reading chatRoom users
+    const users = cache.readQuery<GetChatRoomUsersQuery>({
+      query: GetChatRoomUsersDocument,
+      variables: { roomId },
+    });
+
+    //updating chatRoom users if query exists
+    if (users) {
+      cache.writeQuery<GetChatRoomUsersQuery>({
+        query: GetChatRoomUsersDocument,
+        variables: { roomId },
+        data: {
+          getChatRoomUsers: {
+            ...users!.getChatRoomUsers,
+            others: [...users.getChatRoomUsers.others, userWithAvatar],
+          },
+        },
+      });
+    }
+  };
+};
