7e9200664e6681fdf2a2d6a1dfdc2b91a3b72349
Michał Warchoł <imsimilar@gmail.com>
2021-11-14 23:24:47 +0100

    :floppy_disk: Added prefixes for phone and email keys

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,3 +1,5 @@
 export const __prod__ = process.env.NODE_ENV === "production";
 export const COOKIE_NAME = "uid";
 export const VERIFICATION_PREFIX = "verification-prefix";
+export const UPDATE_PHONE_PREFIX = "update-phone-prefix";
+export const UPDATE_EMAIL_PREFIX = "update-email-prefix";
