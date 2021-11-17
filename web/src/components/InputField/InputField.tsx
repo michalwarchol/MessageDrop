8c2187c2b0070ff0ab7957007479f90ca6dfdd74
Michał Warchoł <imsimilar@gmail.com>
2021-11-17 15:27:35 +0100

    :wrench: Added myRef prop

--- web/src/components/InputField/InputField.tsx
+++ web/src/components/InputField/InputField.tsx
@@ -4,13 +4,14 @@ import styles from "./InputField.module.scss";
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
     name: string;
+    myRef?: React.RefObject<any>
 }
 
-const InputField:React.FC<InputFieldProps> = (props) => {
+const InputField:React.FC<InputFieldProps> = ({myRef, ...props}) => {
     const [field, {error}] = useField(props);
     return(
         <div className={styles.inputContainer}>
-            <input className={styles.inputField} {...field} {...props} />
+            <input className={styles.inputField} {...field} {...props} ref={myRef} />
             {error && <p>{error}</p>}
         </div>
     )
