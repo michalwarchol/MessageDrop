911faf896d3c1016b60ed54f34fb2d6722f50c00
Michał Warchoł <imsimilar@gmail.com>
2021-11-21 14:12:54 +0100

    :telephone_receiver: Added PhoneInput and redirect to /verify after successful registration

--- web/src/pages/register/index.tsx
+++ web/src/pages/register/index.tsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Form } from "formik";
 import NextLink from "next/link";
 import Wrapper from "../../components/Wrapper/Wrapper";
 import styles from "./register.module.scss";
 import InputField from "../../components/InputField/InputField";
+import PhoneField from "../../components/PhoneInput/PhoneInput";
 import Button from "../../components/Button/Button";
-import { useRegisterMutation } from "../../generated/graphql";
+import { FieldError, MeDocument, MeQuery, useRegisterMutation } from "../../generated/graphql";
 import { toErrorMap } from "../../utils/toErrorMap";
 import { useRouter } from "next/dist/client/router";
 import { withApollo } from "../../utils/withApollo";
+import { E164Number } from "libphonenumber-js/types";
+import { isValidPhoneNumber } from "libphonenumber-js/min";
 
 const Register: React.FC = () => {
   const [register, { loading }] = useRegisterMutation();
   const router = useRouter();
+
+  const [phone, setPhone] = useState<E164Number | undefined>(undefined);
+  const [phoneNumberError, setPhoneNumberError] = useState<
+    FieldError | undefined
+  >(undefined);
 
   return (
     <div className={styles.register}>
@@ -23,22 +31,41 @@ const Register: React.FC = () => {
               name: "",
               password: "",
               email: "",
-              phone: "",
               repeatpassword: "",
             }}
             onSubmit={async ({ repeatpassword, ...values }, { setErrors }) => {
+              setPhoneNumberError(undefined);
+              if (!phone || !isValidPhoneNumber(phone as string)) {
+                setPhoneNumberError({
+                  field: "phoneNumber",
+                  message: "This phone number is not valid",
+                });
+                return;
+              }
+
               if (values.password != repeatpassword) {
                 setErrors({
                   repeatpassword: "Repeated password doesn't match",
                 });
                 return;
               }
-              const response = await register({ variables: { ...values } });
+              const response = await register({
+                variables: { ...values, phone: phone as string },
+                update: (cache, { data }) => {
+                  cache.writeQuery<MeQuery>({
+                    query: MeDocument,
+                    data: {
+                      __typename: "Query",
+                      me: data?.register?.user || null,
+                    },
+                  });
+                },
+              });
 
               if (response.data?.register.errors) {
                 setErrors(toErrorMap(response.data.register.errors));
               } else if (response.data?.register.user) {
-                router.push("/home");
+                router.push("/verify");
               }
             }}
           >
@@ -58,11 +85,15 @@ const Register: React.FC = () => {
                   placeholder="email"
                   type="email"
                 />
-                <InputField
+                <PhoneField
+                  defaultCountry="PL"
+                  value={phone}
+                  onChange={setPhone}
                   name="phone"
                   autoComplete="off"
                   placeholder="phone"
                   type="phone"
+                  error={phoneNumberError}
                 />
                 <InputField
                   name="password"
@@ -77,7 +108,16 @@ const Register: React.FC = () => {
                   type="password"
                 />
                 <div className={styles.buttons}>
-                  <Button text="Sign Up" loading={loading} className={styles.submitButton} />
+                  <p className={styles.submitInfo}>
+                    After you submit, we will send you a verification code on
+                    your email
+                  </p>
+                  <Button
+                    text="Sign Up"
+                    loading={loading}
+                    className={styles.submitButton}
+                    type="submit"
+                  />
                   <NextLink href="/login">
                     <p>I already have an account</p>
                   </NextLink>
