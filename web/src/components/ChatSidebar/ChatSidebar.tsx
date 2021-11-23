022803789546b7a4f151af1b1377570395310043
Michał Warchoł <imsimilar@gmail.com>
2021-11-23 14:01:50 +0100

    :recycle: Added loading indicator

--- web/src/components/ChatSidebar/ChatSidebar.tsx
+++ web/src/components/ChatSidebar/ChatSidebar.tsx
@@ -4,21 +4,34 @@ import Divider from "../Divider/Divider";
 import CreateChatButton from "../CreateChatButton/CreateChatButton";
 import { useGetUserChatRoomsQuery } from "../../generated/graphql";
 import ChatRoomShortCut from "../ChatRoomShortcut/ChatRoomShortcut";
+import LoadingIndicator from "../LoadingIndicator/LoadingIndicator";
 
 const ChatSidebar: React.FC = () => {
-  const { data } = useGetUserChatRoomsQuery();
+  const { data, loading } = useGetUserChatRoomsQuery();
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <div className={styles.chatSidebar} style={{zIndex: isOpen ? 99 : 1}}>
+    <div className={styles.chatSidebar} style={{ zIndex: isOpen ? 99 : 1 }}>
       <div className={styles.create}>
         <CreateChatButton isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
       <Divider />
       <div className={styles.userRooms}>
-        {data?.getUserChatRooms.map((elem, index) => (
-          <ChatRoomShortCut chatRoomWithImage={elem} key={index} />
-        ))}
+        {!data && loading && (
+          <div className={styles.loading}>
+            <LoadingIndicator />
+          </div>
+        )}
+
+        {data?.getUserChatRooms
+          .map((elem, index) => (
+            <ChatRoomShortCut chatRoomWithImage={elem} key={index} />
+          ))
+          .reverse()}
+
+        {data && data.getUserChatRooms.length < 1 && !loading && (
+          <div className={styles.loading}>Your rooms</div>
+        )}
       </div>
     </div>
   );
