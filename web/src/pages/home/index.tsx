42ba5ed6d16a29dbe49f96b38152eeb60608d11c
Michał Warchoł <imsimilar@gmail.com>
2021-11-29 17:21:02 +0100

    :hammer: Removed banner

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -2,25 +2,16 @@ import React from "react";
 import styles from "./Home.module.scss";
 import Wrapper from "../../components/Wrapper/Wrapper";
 import Navbar from "../../components/Navbar/Navbar";
-import { useMeQuery } from "../../generated/graphql";
 import ChatSidebar from "../../components/ChatSidebar/ChatSidebar";
 import ExploreContent from "../../components/ExploreContent/ExploreContent";
 import { withApollo } from "../../utils/withApollo";
 import { withAuth } from "../../utils/withAuth";
 
 const Home: React.FC = () => {
-  const { data } = useMeQuery();
-  
   return (
     <Wrapper size="lg">
       <div className={styles.home}>
         <Navbar />
-        <div className={styles.greetings}>
-          <div className={styles.greetingsInner}>
-            <h1>Hi, {data?.me?.name}...</h1>
-            <h1>...wanna talk?</h1>
-          </div>
-        </div>
         <div>
           <Wrapper size="md">
             <div className={styles.content}>
