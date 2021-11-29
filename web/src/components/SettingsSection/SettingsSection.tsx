7df0b321749b461dd2779a2a87cdca415f2f43cc
Michał Warchoł <imsimilar@gmail.com>
2021-11-29 17:35:06 +0100

    :hammer: Refactored

--- web/src/components/SettingsSection/SettingsSection.tsx
+++ web/src/components/SettingsSection/SettingsSection.tsx
@@ -1,19 +1,11 @@
-import { useApolloClient } from "@apollo/client";
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import {
-  ChatRoomWithImage,
-  GetUserChatRoomsDocument,
-  GetUserChatRoomsQuery,
-  IsChatMemberDocument,
-  IsChatMemberQuery,
-  NewChatUsersDocument,
-  NewChatUsersSubscription,
-  NewChatUsersSubscriptionVariables,
   useGetChatRoomByIdQuery,
   useGetChatRoomUsersQuery,
   useMeQuery,
 } from "../../generated/graphql";
 import { RoomContext } from "../../utils/RoomContext";
+import { useChatRoomUsersSub } from "../../utils/useChatRoomUsersSub";
 import { Permissions } from "../../utils/UserPermissions";
 import ChatRoomSettingsForm from "../ChatRoomSettingsForm/ChatRoomSettingsForm";
 import UserNode from "../UserNode/UserNode";
@@ -24,104 +16,14 @@ const SettingsSection: React.FC = () => {
   const { data } = useGetChatRoomByIdQuery({
     variables: { roomId },
   });
-  const { data: users, subscribeToMore } = useGetChatRoomUsersQuery({
+  const { data: users } = useGetChatRoomUsersQuery({
     variables: { roomId },
     fetchPolicy: "cache-and-network",
     nextFetchPolicy: "cache-only"
   });
   const { data: me } = useMeQuery();
-  const apolloClient = useApolloClient();
 
-  useEffect(() => {
-    subscribeToMore<
-      NewChatUsersSubscription,
-      NewChatUsersSubscriptionVariables
-    >({
-      document: NewChatUsersDocument,
-      variables: { roomId },
-      updateQuery: (prev, { subscriptionData }) => {
-        if (!subscriptionData) {
-          return prev;
-        }
-
-        const newUserIds = subscriptionData.data.newChatUsers.userIds;
-        const newModIds = subscriptionData.data.newChatUsers.modIds;
-
-        if (
-          prev.getChatRoomUsers.admin.user._id != me!.me!._id && 
-          !newUserIds.find((elem) => elem == me!.me!._id) &&
-          !newModIds.find((elem) => elem == me!.me!._id)
-        ) {
-          apolloClient.cache.writeQuery<IsChatMemberQuery>({
-            variables: {roomId},
-            query: IsChatMemberDocument,
-            data: {
-              isChatMember: false
-            }
-          });
-
-          const myRooms = apolloClient.cache.readQuery<GetUserChatRoomsQuery>({
-            query: GetUserChatRoomsDocument
-          });
-          let myNewRooms = myRooms?.getUserChatRooms.filter(elem=> elem.chatRoom._id != roomId);
-
-          apolloClient.cache.writeQuery<GetUserChatRoomsQuery>({
-            query: GetUserChatRoomsDocument,
-            data: {
-              getUserChatRooms: myNewRooms as ChatRoomWithImage[]
-            }
-          })
-        }
-
-        //get only existing users
-        let others = prev.getChatRoomUsers.others.filter((elem) =>
-          newUserIds.includes(elem.user._id)
-        );
-        let mods = prev.getChatRoomUsers.mods.filter((elem) =>
-          newModIds.includes(elem.user._id)
-        );
-
-        //get missing ids
-        const missingOthers = newUserIds.filter(
-          (elem) => !others.find((user) => user.user._id == elem)
-        );
-        const missingMods = newModIds.filter(
-          (elem) => !mods.find((user) => user.user._id == elem)
-        );
-
-        //add missing ids
-        others = others.concat(
-          prev.getChatRoomUsers.mods.filter((elem) =>
-            missingOthers.find((id) => id == elem.user._id)
-          )
-        );
-        mods = mods.concat(
-          prev.getChatRoomUsers.others.filter((elem) =>
-            missingMods.find((id) => id == elem.user._id)
-          )
-        );
-
-        //add a new user
-        if (subscriptionData.data.newChatUsers.newUser) {
-          let newUser = subscriptionData.data.newChatUsers.newUser;
-          if (newUserIds.includes(newUser.user._id)) {
-            others.push(newUser);
-          } else if (newModIds.includes(newUser.user._id)) {
-            mods.push(newUser);
-          }
-        }
-
-        return {
-          getChatRoomUsers: {
-            admin: prev.getChatRoomUsers.admin,
-            mods,
-            others,
-          },
-          __typename: prev.__typename,
-        };
-      },
-    });
-  }, []);
+  useChatRoomUsersSub(roomId);
 
   return (
     <div className={styles.settingsSection}>
